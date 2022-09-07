e578a853889c6011f5a6a7bbbf0d5667c310268d
Kollin7 <113071525+Kollin7@users.noreply.github.com>
2022-09-07 18:30:49 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "your_league_id"; // 870103646174666752
+export const leagueName = "your_league_name"; // Kollin's Dynasty League
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
